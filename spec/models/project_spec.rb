0f6a460ea8ccaf1f426f7f248f7fa6498d955489
Mohnish G J <mail4mohnish@gmail.com>
2020-11-12 11:44:59 +0100

    Add rubocop linting fixes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,29 +6,33 @@ RSpec.describe Project, type: :model do
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:owner_id) }
     it do
-      should define_enum_for(:status).with_values([:planned, :active, :done, :failed]).with_prefix
+      should define_enum_for(:status).with_values(%i[planned active done failed]).with_prefix
     end
 
-    context "owner is manager validation" do
-      context "Project owner is not an employee with a manager role" do
-        let(:project_record) { Project.new(name: 'new_project', owner_id: "b82522f0-8644-4c65-a552-9c6b8a9e4b7a",
-                                        status: 'active' ) }
+    context 'owner is manager validation' do
+      context 'Project owner is not an employee with a manager role' do
+        let(:project_record) do
+          Project.new(name: 'new_project', owner_id: 'b82522f0-8644-4c65-a552-9c6b8a9e4b7a',
+                      status: 'active')
+        end
 
-        it "fails validation and returns an appropriate error message" do
+        it 'fails validation and returns an appropriate error message' do
           employee_service = double('employee_service')
           allow(EmployeeService).to receive(:new) { employee_service }
           allow(employee_service).to receive(:role).with(project_record.owner_id) { 'employee' }
 
           project_record.validate
-          expect(project_record.errors.full_messages).to eq(["Owner is not manager This record is invalid"])
+          expect(project_record.errors.full_messages).to eq(['Owner is not manager This record is invalid'])
         end
       end
 
-      context "Project owner is an employee with a manager role" do
-        let(:project_record) { Project.new(name: 'new_project', owner_id: "f3e868b9-6151-49c4-8d51-b9e3fb9255ba",
-                                          status: 'active' ) }
+      context 'Project owner is an employee with a manager role' do
+        let(:project_record) do
+          Project.new(name: 'new_project', owner_id: 'f3e868b9-6151-49c4-8d51-b9e3fb9255ba',
+                      status: 'active')
+        end
 
-        it "returns true" do
+        it 'returns true' do
           employee_service = double('employee_service')
           allow(EmployeeService).to receive(:new) { employee_service }
           allow(employee_service).to receive(:role).with(project_record.owner_id) { 'manager' }
--- spec/requests/api/v1/projects_request_spec.rb
+++ spec/requests/api/v1/projects_request_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Projects", type: :request do
+RSpec.describe 'Api::V1::Projects', type: :request do
   describe 'POST /projects' do
     context 'required params are not present' do
       it 'returns with the appropriate error message and status code' do
-        project_params = { project: { name: 'new project'} }
+        project_params = { project: { name: 'new project' } }
 
         post '/api/v1/projects', params: project_params
 
@@ -18,7 +18,7 @@ RSpec.describe "Api::V1::Projects", type: :request do
     context 'required params are present' do
       context 'Project params has valid data' do
         it 'returns the created idea info and the appropriate status code' do
-          project_params = { project: { name: 'new project', status: 'planned', owner_id: 'f4587606-4504-453c-8e7b-fe6029847d41'} }
+          project_params = { project: { name: 'new project', status: 'planned', owner_id: 'f4587606-4504-453c-8e7b-fe6029847d41' } }
 
           expect { post '/api/v1/projects', params: project_params }.to change { Project.count }.by(1)
 
@@ -36,7 +36,7 @@ RSpec.describe "Api::V1::Projects", type: :request do
 
       context 'Project params has invalid data' do
         it 'returns the appropriate error message and status code' do
-          project_params = { project: { name: '', status: 'planned', owner_id: 'f4587606-4504-453c-8e7b-fe6029847d41'} }
+          project_params = { project: { name: '', status: 'planned', owner_id: 'f4587606-4504-453c-8e7b-fe6029847d41' } }
 
           expect { post '/api/v1/projects', params: project_params }.to change { Project.count }.by(0)
 
@@ -44,7 +44,7 @@ RSpec.describe "Api::V1::Projects", type: :request do
 
           expect(@response.status).to eq(422)
           expect(parsed_response_body['errors']).to eq(
-            'name' => ["can't be blank"],
+            'name' => ["can't be blank"]
           )
         end
       end
